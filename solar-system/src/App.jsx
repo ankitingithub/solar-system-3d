49de2c2a98e892ab248e38921eb9968e64aa6cba
ankitingithub <as0617300@gmail.com>
2025-06-10 11:26:45 +0530

    bug fix in appjs

--- solar-system/src/App.jsx
+++ solar-system/src/App.jsx
@@ -1,6 +1,17 @@
-<>
-  <Controls speeds={speeds} setSpeeds={setSpeeds} />
-  <div style={{ width: '100vw', height: '100vh' }}>
-    <SolarSystem speeds={speeds} />
-  </div>
-</>
+import { useState } from "react";
+import SolarSystem from "./components/SolarSystem";
+import Controls from "./components/Controls";
+import "./styles.css";
+
+export default function App() {
+  const [speeds, setSpeeds] = useState([1, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2]);
+
+  return (
+    <>
+      <Controls speeds={speeds} setSpeeds={setSpeeds} />
+      <div style={{ width: "100vw", height: "100vh" }}>
+        <SolarSystem speeds={speeds} />
+      </div>
+    </>
+  );
+}
